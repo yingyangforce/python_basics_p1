fe37c3f5817749c314e1ee37446fa3aebbe46e49
Gene Calmer <yingyangforce@gmail.com>
2022-08-04 09:53:32 -0400

    feat: finish p2

---
+++ p2.py
@@ -0,0 +1,9 @@
+#---------------------------------------
+# | p2 - | print current python version
+#---------------------------------------
+
+import sys
+
+print(f"Current Python version: {sys.version}")
+
+
