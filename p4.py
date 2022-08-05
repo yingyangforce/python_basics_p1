73fb9f9286f8b95ae5460eaea9e07de7471e9d2e
Gene Calmer <yingyangforce@gmail.com>
2022-08-05 09:23:05 -0400

    feat: figure out python argv

---
+++ p4.py
@@ -0,0 +1,9 @@
+#-----------------------------------------------------
+# | p4 - | get radius from user, print area of circle
+#-----------------------------------------------------
+
+import sys
+
+if __name__ == '__main__':
+    print(sys.argv)
+
