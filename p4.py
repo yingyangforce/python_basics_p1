d80ff32bb01dbaa08742bb49e7d7739a3d5ce53a
Gene Calmer <yingyangforce@gmail.com>
2022-08-07 18:21:55 -0400

    refactor(p4): rm name == main check

--- p4.py
+++ p4.py
@@ -4,16 +4,15 @@
 
 import sys, math
 
-if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        radius = float(sys.argv[1])
-    else:
-        radius = float(input("Enter the radius of your circle: "))
+if len(sys.argv) > 1:
+    radius = float(sys.argv[1])
+else:
+    radius = float(input("Enter the radius of your circle: "))
 
-    if radius == int(radius):
-        radius = int(radius)
+if radius == int(radius):
+    radius = int(radius)
 
-    area = math.pi * (radius ** 2)
-    print(f"Area of circle for r={radius} -> {area:.3f}")
+area = math.pi * (radius ** 2)
+print(f"Area of circle for r={radius} -> {area:.3f}")
 
 
