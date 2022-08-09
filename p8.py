5bffb3d8fb895b31442e6cb42d0b6965d40a3645
Gene Calmer <yingyangforce@gmail.com>
2022-08-09 02:37:47 -0400

    print from list

---
+++ p8.py
@@ -0,0 +1,8 @@
+#----------------------------------------------------
+# | p8 - | print first, last element of example list
+#----------------------------------------------------
+
+color_list = ['Red', 'Green', 'White', 'Black']
+
+print(f"{color_list[0]}, {color_list[-1]}")
+
